df95e9b83f4b2b43ad56971bb33a6659c2525b16
ASPP Student <student@localhost>
2022-09-05 17:10:41 +0200

    Work in progress

--- auth.py
+++ auth.py
@@ -1,9 +1,15 @@
 import json
+import random
+import string
+
+
+def random_string():
+    s = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(1000))
+    return s
 
 def get_credentials():
     username = input('Enter your username: ')
-    password = pwhash(input(f'Enter your password {username}: '))
-
+    password = input(f'Enter your password {username}: ')
     return username, password
 
 def read_passwdb():
@@ -15,20 +21,24 @@ def write_passwdb(pwdb):
     with open('passwd.json', 'w') as pwdb_file:
         json.dump(pwdb, pwdb_file)
 
-def pwhash(password):
+def pwhash(salt, password):
     pwh = 0
-    for i, char in enumerate(password):
+    to_hash = salt + password
+    for i, char in enumerate(to_hash):
         pwh += (i + 1) * ord(char)
-    return pwh
+    return str(pwh)
 
 def add_user(pwdb, username, password):
     if username not in pwdb:
-        pwdb[username] = password
-
+        salt = random_string()
+        hashed_pass = pwhash(salt, password)
+        pwdb[username] = salt, hashed_pass
 
 def authenticate(username, password, pwdb):
     if username in pwdb:
-        if password == pwdb[username]:
+        salt, password = pwdb[username]
+        hashed_password = pwhash(salt, password)
+        if hashed_password == password:
             return True
         else:
             return False
